a10b836178971afd513594f4c2570203a1949be5
vic <vic@localhost>
2024-01-15 19:57:49 +0100

    Initial commit

---
+++ src/main/java/com/vic/chomporder/ChompOrderApplication.java
@@ -0,0 +1,13 @@
+package com.vic.chomporder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChompOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChompOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vic/chomporder/ChompOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vic.chomporder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChompOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
