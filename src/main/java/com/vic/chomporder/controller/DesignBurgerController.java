1283745b9a4900b216f52e3147ac4bfd89eccebd
vic <vic@localhost>
2024-01-19 06:54:12 +0100

    Design burger form HTML added

--- src/main/java/com/vic/chomporder/controller/DesignBurgerController.java
+++ src/main/java/com/vic/chomporder/controller/DesignBurgerController.java
@@ -17,7 +17,6 @@ import com.vic.chomporder.model.Ingredient.Type;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Slf4j
